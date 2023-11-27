fab4d802acdfe71a283f2349416dc3c2859c9fab
BreinerVirguez <127887239+BreinerVirguez@users.noreply.github.com>
2023-11-27 09:07:06 -0500

    main.js

--- js/main.js
+++ js/main.js
@@ -4,47 +4,58 @@ let button = document.getElementById("btn");
 
 function circulo(){
     figura.classList.toggle("circulo");
+    titulo.innerText = "Cambiar a circulo";
 }
 
 function moveTop(){
     figura.classList.toggle("moveTop");
+    titulo.innerText = "Cambiar a moveTop";
 }
 
 
 function rectangulo(){
     figura.classList.toggle("rectangulo");
+   titulo.innerText = "Cambiar a rectangulo";
 }
 
 function rombo(){
     figura.classList.toggle("rombo");
+    titulo.innerText = "Cambiar a rombo";
 }
 
 function imagen(){
     figura.classList.toggle("imagen");
+   titulo.innerText = "Cambiar a imagen";
 }
 
 function imgif(){
     figura.classList.toggle("imgif");
+   titulo.innerText = "Cambiar a gif";
 }
 
 function triangulo(){
     figura.classList.toggle("triangulo");
+   titulo.innerText = "Cambiar a triangulo";
 }
 
 function huevo(){
     figura.classList.toggle("huevo");
+   titulo.innerText = "Cambiar a huevo";
 }
 
 function pacMan(){
     figura.classList.toggle("pacMan");
+   titulo.innerText = "Cambiar a pacMan";
 }
 
 function luna(){
     figura.classList.toggle("luna");
+   titulo.innerText = "Cambiar a luna";
 }
 
 function hoja(){
     figura.classList.toggle("hoja");
+   titulo.innerText = "Cambiar a hoja";
 }
 
 function mostrarPanelLateral() {
@@ -59,18 +70,22 @@ function cerrarPanelLateral() {
 
 function trapecio(){
     figura.classList.toggle("trapecio");
+   titulo.innerText = "Cambiar a trapecio";
 }
 
 function moveLeft(){
     figura.classList.toggle("moveLeft");
+ titulo.innerText = "Cambiar a moveLeft";
 }
 
 function moveBottom(){
     figura.classList.toggle("moveBottom");
+ titulo.innerText = "Cambiar a moveBottom";
 }
 
 function moveRight(){
     figura.classList.toggle("moveRight");
+ titulo.innerText = "Cambiar a moveRight";
 }
 
 function mostrarPanelSuperior() {
@@ -82,20 +97,11 @@ function cerrarPanelSuperior() {
     const panelSuperior = document.querySelector('.panel-superior');
     panelSuperior.classList.remove('active');
 }
-function mostrarPanelinferior() {
-    const panelinferior = document.querySelector('.panel-inferior');
-    panelinferior.classList.toggle('active');
-}
-
-function cerrarPanelinferior() {
-    const panelinferior = document.querySelector('.panel-inferior');
-    panelinferior.classList.remove('active');
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     var cambiarBtn = document.getElementById('cambiarBtn');
     cambiarBtn.addEventListener('click', function () {
         document.body.classList.toggle('cambio');
+         titulo.innerText = "Cambiar a Cambiar Fondo";
     });
 });
 function cambiarColor() {
@@ -104,6 +110,7 @@ function cambiarColor() {
 
     leftElement.classList.toggle('colorCambiado');
     rightElement.classList.toggle('colorCambiado');
+     titulo.innerText = "Cambiar a cambiarColor";
 }
 function propuesta() {
     var leftElement = document.getElementById('leftElement');
@@ -111,5 +118,6 @@ function propuesta() {
 
     leftElement.classList.toggle('colorCambiado1');
     rightElement.classList.toggle('colorCambiado2');
+     titulo.innerText = "Cambiar a Propuesta";
 }
 
